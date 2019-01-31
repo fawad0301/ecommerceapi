dadc266aa6982ebb05e38091431c0d3056f7a872
Fawad Ali <fawad0301@yahoo.com>
2019-01-31 12:51:11 +0500

    Product reivew created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -5,9 +5,12 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use App\Model\Product;
 
-
 class Review extends Model
 {
+    protected $fillable =[
+        'customer', 'star', 'review'
+    ];
+    
     public function product()
     {
         return $this->belongsTo(Product::class);
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('/products', 'ProductController');
 Route::group(['prefix' =>'products'], function () {
-    Route::apiResource('/{product}/review', 'ReviewController');
+    Route::apiResource('/{product}/reviews', 'ReviewController');
 });
